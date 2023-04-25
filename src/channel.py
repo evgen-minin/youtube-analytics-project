abbb81eb85ffc44107ec1bf9018a94bee25dc426
Евгений Минин <evgenminin1988@mail.ru>
2023-04-25 23:14:24 +0300

    'completed homework-3'

--- src/channel.py
+++ src/channel.py
@@ -15,9 +15,57 @@ class Channel:
         self.title = self.channel['items'][0]['snippet']['title']
         self.channel_description = self.channel['items'][0]['snippet']['description']
         self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
-        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
-        self.video_count = self.channel['items'][0]['statistics']['videoCount']
-        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+        self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
+        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
+
+    def __str__(self) -> str:
+        """
+        Возвращает строковое представление объекта класса Channel.
+        """
+        return f"'{self.title} ({self.url})'"
+
+    def __add__(self, other) -> int:
+        """
+        Метод для операции сложения
+        """
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other) -> int:
+        """
+        Метод для операции вычитания
+        """
+        return self.subscriber_count - other.subscriber_count
+
+    def __gt__(self, other) -> bool:
+        """
+        Метод для операции сравнения больше
+        """
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other) -> bool:
+        """
+        Метод для операции сравнения больше или равно
+        """
+        return self.subscriber_count >= other.subscriber_count
+
+    def __lt__(self, other) -> bool:
+        """
+        Метод для операции сравнения меньше
+        """
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other) -> bool:
+        """
+        Метод для операции сравнения меньше или равно
+        """
+        return self.subscriber_count <= other.subscriber_count
+
+    def __eq__(self, other) -> bool:
+        """
+        Метод для операции сравнения равенства
+        """
+        return self.subscriber_count == other.subscriber_count
 
     @property
     def channel_id(self) -> str:
